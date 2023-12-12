a67930e2290495022bef9f946b4ec5e1fc1240e7
Bohdan Agarkov <keell0renz@localhost.localdomain>
2023-12-12 19:42:09 +0100

    Disabled middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default function middleware(req: NextRequest) {
-    console.log(`Middleware for ${req.url}`)
+    // console.log(`Middleware for ${req.url}`)
 
-    const response = NextResponse.next()
+    // const response = NextResponse.next()
 
-    if (req.cookies.has("my-nextjs-app")) {
-        console.log("Custom cookie found!")
-    } else {
-        response.cookies.set("my-nextjs-app", "value")
-    }
+    // if (req.cookies.has("my-nextjs-app")) {
+    //     console.log("Custom cookie found!")
+    // } else {
+    //     response.cookies.set("my-nextjs-app", "value")
+    // }
 
-    return response
+    // return response
 }
 
 export const config = {
